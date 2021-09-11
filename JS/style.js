63f88883351e87b2c194c4800eb0893d980b78f1
umerkhan07 <77659546+umerkhan07@users.noreply.github.com>
2021-09-11 16:25:12 +0500

    Update style.js

--- JS/style.js
+++ JS/style.js
@@ -6,6 +6,11 @@ const title = document.getElementById("title");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
 
+let progress = document.getElementById("progress"); //updated
+let total_duration = document.getElementById("duration");
+let current_time = document.getElementById("current_time");
+const progress_div = document.getElementById("progress_div");
+
 const songs = [{
         name: "kun-faya-kun",
         artist: "Hurrain Fatima",
@@ -79,6 +84,44 @@ const prevSong = () => {
     loadSong(songs[songIndex]);
     playMusic();
 };
+
+//progress bar 
+
+music.addEventListener("timeupdate", (event) => {
+
+    const { currentTime, duration } = event.srcElement;
+    let progress_time = (currentTime / duration) * 100;
+    progress.style.width = `${progress_time}% `;
+
+    let min_duration = Math.floor(duration / 60);
+    let sec_duration = Math.floor(duration % 60);
+
+    let tot_duration = `${min_duration}:${sec_duration}`;
+    if (duration) {
+        total_duration.textContent = `${tot_duration}`;
+    }
+
+    let min_currentTime = Math.floor(currentTime / 60);
+    let sec_currentTime = Math.floor(currentTime % 60);
+
+    if (sec_currentTime < 10) {
+        sec_currentTime = `${sec_currentTime}`;
+    }
+    let tot_currentTime = `${min_currentTime}:${sec_currentTime}`;
+
+    current_time.textContent = `${tot_currentTime}`;
+
+
+});
+
+progress_div.addEventListener("click", (event) => {
+    console.log(event);
+    const { duration } = music;
+    let move_progress = (event.offsetX / event.srcElement.clientWidth) * duration;
+
+    music.currentTime = move_progress;
+});
+music.addEventListener("ended", nextSong)
 
 next.addEventListener("click", nextSong);
 prev.addEventListener("click", prevSong);
